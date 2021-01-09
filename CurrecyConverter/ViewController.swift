7e9fa4c90c74768822dda8d2fec05179f2825683
ghigbie <gehigbie@gmail.com>
2021-01-08 23:48:05 -0500

    Add Outlet and actions to view controller

--- CurrecyConverter/ViewController.swift
+++ CurrecyConverter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var converted: UILabel!
+    
+    @IBOutlet weak var conversionRate: UITextField!
+    
+    @IBOutlet weak var conversionAmount: UITextField!
+    
+    @IBOutlet weak var currency: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func convert(_ sender: Any) {
+        let rate = Double(conversionRate.text!)!
+        let amount = Double(conversionAmount.text!)!
+    }
+    
 }
 
