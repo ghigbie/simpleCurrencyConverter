5870010867c6401f546fc3592d9aed7afbabb5b3
ghigbie <gehigbie@gmail.com>
2021-01-08 22:29:14 -0500

    Initial commit

---
+++ CurrecyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrecyConverter
+//
+//  Created by George Higbie on 1/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
